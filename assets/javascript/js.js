5a1401c84c8305bb25e43ae0c34119d747dfe81e
David Gallegos <david.gallegos@outlook.com>
2019-04-10 18:58:10 -0700

    updates from david

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,9 +1,12 @@
 
 // make sure ajax call is parent of this function
-$("#searchButton").click(function(){
-    event.preventDefault()
-    var search = $("#input").val()
-})
+$(document).ready(function () {
+    $("#searchButton").click(function () {
+        event.preventDefault()
+        var search = $("#input").val()
+        console.log(search);
 
+    })
+})
 
 
